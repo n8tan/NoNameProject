45cee802663df11345e71170a957fc8d2fe5aeaa
Nathan R <Nathan.Remante@finastra.com>
2020-02-17 08:03:13 +0800

    Enhanced printOutList to print contents of RegressionList in boxes. Might need to rework the logic but for now it works :). Added another method called computeMaximumBoxWidth that gets maximum width needed to print a box.

--- venv/RegressionListMaker.py
+++ venv/RegressionListMaker.py
@@ -5,11 +5,24 @@ class ListManager:
     def addToList(self, name):
         self.RegressionList.append(name)
 
+    def computeMaximumBoxWidth(self):
+        longestLength = 0
+        for entry in self.RegressionList:
+            if longestLength < len(entry):
+                longestLength = len(entry)
+        return longestLength
+
     def printOutList(self):
+        maxLength = self.computeMaximumBoxWidth()
+        prefixTextToAdd = " Build name: "
+        maxLength += len(prefixTextToAdd)
+        padding = 0
         for name in self.RegressionList:
-            print("------------------")
-            print("Build name:" + name)
-        print("------------------")
+            print("+" + "-"*maxLength + "+")
+            padding = len(name)
+            padding = maxLength - padding - len(prefixTextToAdd)
+            print("|" + " Build name: " + name + " "*padding + "|")
+        print("+" + "-"*maxLength + "+")
 
     def insertIntoList(self, name, position):
         position -= 1
@@ -18,12 +31,15 @@ class ListManager:
     def removeFromList(self, name):
         self.RegressionList.remove(name)
 
+
+
 print("Starting")
 manager = ListManager()
 manager.addToList("bob")
 manager.addToList("shark")
 manager.addToList("Jam")
 manager.addToList("Potato")
+manager.addToList("Crocodile")
 manager.printOutList()
 
 manager.insertIntoList("couch",2)
